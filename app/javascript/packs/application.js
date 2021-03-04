060c9c92850172a3d94d1fa789c7d4032bf72652
Lucien George <luciengeorge95@gmail.com>
2021-03-04 22:54:05 +0000

    removed stimulus hello_controller + setup tailwindcss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "controllers";
+import "stylesheets/application";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import "controllers";
-
-import "stylesheets/application";
--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,11 +1,32 @@
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    preflight: true
+  },
   theme: {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      margin: ['important'],
+      fontWeight: ['important'],
+      borderRadius: ['important']
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    plugin(function({ addVariant }) {
+      addVariant('important', ({ container }) => {
+        container.walkRules(rule => {
+          rule.selector = `.\\!${rule.selector.slice(1)}`
+          rule.walkDecls(decl => {
+            decl.important = true
+          })
+        })
+      })
+    })
+  ],
 }
