7775a3ddb8e684662c22b7aaafcde840e5ecbb4c
Lucien George <luciengeorge95@gmail.com>
2021-03-04 22:54:51 +0000

    root to pages#home

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,16 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = false
+    Bullet.bullet_logger = false
+    Bullet.console       = false
+  # Bullet.growl         = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'http://localhost:3000' }
   # In the development environment your application's code is reloaded any time
