fbfe08026ece970aff00840bc16cb7fc49fc9272
Lucien George <luciengeorge95@gmail.com>
2021-03-06 21:24:21 +0000

    configured mailer in dev and prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'https://www.lucien-george.com' }
+  config.action_mailer.delivery_method = :postmark
   # Code is not reloaded between requests.
   config.cache_classes = true
 
