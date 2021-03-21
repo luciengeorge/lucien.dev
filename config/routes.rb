c971ce82e91c18bd7fafaae74481fde4b92ed811
Lucien George <luciengeorge95@gmail.com>
2021-03-21 12:37:42 +0000

    sidekiq setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :registrations
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
   root to: 'pages#home'
   resources :projects, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
