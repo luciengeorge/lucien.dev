064e347dc5d3c1c9d89fa98af338435e24f63343
Lucien George <luciengeorge95@gmail.com>
2021-03-22 18:08:46 +0000

    finished jobs to update collaborators, repos and languages

--- app/jobs/update_repo_job.rb
+++ app/jobs/update_repo_job.rb
@@ -2,49 +2,52 @@ class UpdateRepoJob < ApplicationJob
   queue_as :default
 
   def perform(repo_gh_id)
-    @repo = Repo.find_by gh_id: repo_gh_id
+    @repo_gh_id = repo_gh_id
+    @repo = Repo.find_by gh_id: @repo_gh_id
+    @github_repo = $octokit.repo(@repo_gh_id)
     @repo.present? ? update_repo : create_repo
     UpdateRepoLanguagesJob.perform_later(@repo)
+    UpdateRepoCollaboratorsJob.perform_later(@repo)
   end
 
   private
 
   def update_repo
     @repo.update(
-      name: repo.name,
-      full_name: repo.full_name,
-      private: repo.private,
-      owner: repo.owner,
-      html_url: repo.html_url,
-      api_url: repo.url,
-      fork: repo.fork,
-      pushed_at: repo.pushed_at,
-      git_url: repo.git_url,
-      homepage: repo.homepage,
-      size: repo.size,
-      stars: repo.stargazers_count,
-      forks: repo.forks_count,
-      open_issues: repo.open_issues_count
+      name: @github_repo.name,
+      full_name: @github_repo.full_name,
+      private: @github_repo.private,
+      owner: @github_repo.owner,
+      html_url: @github_repo.html_url,
+      api_url: @github_repo.url,
+      fork: @github_repo.fork,
+      pushed_at: @github_repo.pushed_at,
+      git_url: @github_repo.git_url,
+      homepage: @github_repo.homepage,
+      size: @github_repo.size,
+      stars: @github_repo.stargazers_count,
+      forks: @github_repo.forks_count,
+      open_issues: @github_repo.open_issues_count
     )
   end
 
   def create_repo
     @repo = Repo.create(
-      gh_id: repo_gh_id,
-      name: repo.name,
-      full_name: repo.full_name,
-      private: repo.private,
-      owner: repo.owner,
-      html_url: repo.html_url,
-      api_url: repo.url,
-      fork: repo.fork,
-      pushed_at: repo.pushed_at,
-      git_url: repo.git_url,
-      homepage: repo.homepage,
-      size: repo.size,
-      stars: repo.stargazers_count,
-      forks: repo.forks_count,
-      open_issues: repo.open_issues_count
+      gh_id: @repo_gh_id,
+      name: @github_repo.name,
+      full_name: @github_repo.full_name,
+      private: @github_repo.private,
+      owner: @github_repo.owner,
+      html_url: @github_repo.html_url,
+      api_url: @github_repo.url,
+      fork: @github_repo.fork,
+      pushed_at: @github_repo.pushed_at,
+      git_url: @github_repo.git_url,
+      homepage: @github_repo.homepage,
+      size: @github_repo.size,
+      stars: @github_repo.stargazers_count,
+      forks: @github_repo.forks_count,
+      open_issues: @github_repo.open_issues_count
     )
   end
 end
--- app/jobs/update_repo_languages_job.rb
+++ app/jobs/update_repo_languages_job.rb
@@ -2,6 +2,20 @@ class UpdateRepoLanguagesJob < ApplicationJob
   queue_as :default
 
   def perform(repo)
-    repo.rels[:languages].get.data
+    @repo = $octokit.repo(repo.gh_id)
+    languages = @repo.rels[:languages].get.data
+    languages.each do |language, size|
+      @language = Language.find_or_create_by(name: language.to_s)
+      repo_language = RepoLanguage.find_by repo: repo, language: @language
+      if repo_language.present?
+        repo_language.update(size: size)
+      else
+        RepoLanguage.create(
+          repo: repo,
+          language: @language,
+          size: size
+        )
+      end
+    end
   end
 end
