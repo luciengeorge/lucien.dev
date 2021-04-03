317ec10660e1b600d8302c8e6b2354dd51dac5b9
Lucien George <luciengeorge95@gmail.com>
2021-04-03 22:36:16 +0100

    autmatically generated tests

---
+++ test/components/repos/contributor_modal_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Repos::ContributorModalComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Repos::ContributorModalComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
