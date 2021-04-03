5aa472e9909409d11645b9f10753d1b4a706b547
Lucien George <luciengeorge95@gmail.com>
2021-04-03 22:36:57 +0100

    bug fix

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -6,6 +6,5 @@ class ReposController < ApplicationController
                                            .left_outer_joins(:contributors)
                                            .where.not('full_name LIKE ? and private = ?', 'lewagon%', true)
                                            .where(contributors: { login: Contributor::ME }).order(pushed_at: :desc))
-    @repos = @repos.uniq
   end
 end
