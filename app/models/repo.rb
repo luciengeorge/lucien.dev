9a371e5648481d9ca60dda8bad04e08c5c0af71f
Lucien George <luciengeorge95@gmail.com>
2021-04-03 22:36:40 +0100

    update association

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -5,7 +5,7 @@ class Repo < ApplicationRecord
 
   store :owner, accessors: %i[login id avatar_url html_url type], prefix: true, coder: JSON
 
-  has_many :repo_contributors, -> { joins(:contributor).order('contributors.contribution_count desc') }
+  has_many :repo_contributors, -> { order 'contribution_count desc' }
   has_many :contributors, through: :repo_contributors
   has_many :repo_languages, -> { order 'size desc' }
   has_many :languages, through: :repo_languages
