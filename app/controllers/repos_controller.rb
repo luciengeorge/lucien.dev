a1fadd2e7b5a7889d8fb4a137eb11931353f661e
Lucien George <luciengeorge95@gmail.com>
2021-04-03 21:46:18 +0100

    getting correct repos

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -2,6 +2,10 @@ class ReposController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @pagy, @repos = pagy(policy_scope(Repo).includes(:repo_languages))
+    @pagy, @repos = pagy(policy_scope(Repo).includes(:repo_languages, :languages, :contributors, :repo_contributors)
+                                           .left_outer_joins(:contributors)
+                                           .where.not('full_name LIKE ? and private = ?', 'lewagon%', true)
+                                           .where(contributors: { login: Contributor::ME }).order(pushed_at: :desc))
+    @repos = @repos.uniq
   end
 end
