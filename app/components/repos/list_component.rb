b8c3eefea1e8308300fa8ba998ab11637174f779
Lucien George <luciengeorge95@gmail.com>
2021-04-04 15:32:50 +0100

    infinite scroll for repos

---
+++ app/components/repos/list_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Repos::ListComponent < ViewComponent::Base
+  attr_reader :repos
+
+  def initialize(repos:)
+    @repos = repos
+  end
+
+end
--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -6,6 +6,15 @@ class ReposController < ApplicationController
                                            .left_outer_joins(:contributors)
                                            .where.not('full_name LIKE ? and private = ?', 'lewagon%', true)
                                            .where(contributors: { login: Contributor::ME }).order(pushed_at: :desc))
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          entries: render_to_string(Repos::ListComponent.new(repos: @repos)),
+          pagination: view_context.pagy_nav(@pagy)
+        }
+      end
+    end
   rescue Pagy::OverflowError => e
     redirect_to repos_path(page: 1), alert: "Page #{e.message.split('got').last.strip} doesn't exist"
   end
