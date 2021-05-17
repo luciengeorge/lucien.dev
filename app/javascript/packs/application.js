fbaedde7f23d4c25af32d0e24bb7204b14469f78
Lucien George <luciengeorge95@gmail.com>
2021-05-17 22:56:56 +0100

    refacto + actiontext setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,23 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+require("trix");
+require("@rails/actiontext");
+
 import "controllers";
 import AOS from 'aos';
 import 'stylesheets/tailwind/before.scss';
 import 'stylesheets/application.scss';
 import 'stylesheets/tailwind/after.scss';
 import 'aos/dist/aos.css';
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
 
 document.addEventListener('turbolinks:load', () => {
   AOS.init();
 });
-
-require("trix")
-require("@rails/actiontext")
