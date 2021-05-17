30c06e873f89491bd6808e2d040f46e096875725
Lucien George <luciengeorge95@gmail.com>
2021-05-17 22:55:53 +0100

    wip companies#new and #create

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,10 +2,28 @@ class CompaniesController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     @companies = policy_scope(Company).order(:name)
-    authorize current_user, policy_class: Company
+    authorize current_user, policy_class: CompanyPolicy
   end
 
   def new
-    skip_authorization
+    @company = Company.new
+    authorize @company
+  end
+
+  def create
+    @company = Company.new(company_params)
+    authorize @company
+    if @company.save
+      redirect_to companies_path
+    else
+      flash[:alert] = @company.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :website, :address, :photo)
   end
 end
