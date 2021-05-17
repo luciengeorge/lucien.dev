4cea3bb5fbdab6c4054a641ccec3dac0f7c3ec8c
Lucien George <luciengeorge95@gmail.com>
2021-05-17 22:56:37 +0100

    keys for aws + route for cdn cloudfront

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,11 @@ Rails.application.routes.draw do
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
   end
+  direct :cdn do |blob| # use cdn_url() helper
+    File.join(Rails.application.credentials.aws[:cloudfront_url], blob.key)
+  end
   root to: 'pages#home'
   resources :repos, only: :index
-  get :cv, to: 'pages#cv', as: :cv
-  resources :companies, except: :show do
-    resources :expriences, only: [ :new, :create ]
-  end
-  resources :experiences, only: [ :index, :edit, :update, :destroy ]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :companies, except: :show
+  resources :experiences
 end
