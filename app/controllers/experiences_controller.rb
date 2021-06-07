d23f1cc7a5abfb18aaaab765fa0d8c82f8d33d4b
Lucien George <luciengeorge95@gmail.com>
2021-06-07 16:57:16 +0100

    experiences#create

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,11 +2,27 @@ class ExperiencesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @experiences = policy_scope(Experience)
+    @experiences = policy_scope(Experience).includes(:company)
   end
 
   def new
     @experience = Experience.new
     authorize @experience
+  end
+
+  def create
+    @experience = Experience.new(experience_params)
+    authorize @experience
+    if @experience.save
+      redirect_to experiences_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def experience_params
+    params.require(:experience).permit(:title, :start_date, :end_date, :company_id, :description)
   end
 end
