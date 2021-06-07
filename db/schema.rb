9329d142cd13f8d6c8b59fdf2c2de11e1fea2bea
Lucien George <luciengeorge95@gmail.com>
2021-06-07 17:40:50 +0100

    added job_type to experiences

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_113056) do
+ActiveRecord::Schema.define(version: 2021_06_07_162800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_04_17_113056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "company_id", null: false
+    t.integer "job_type", default: 0, null: false
     t.index ["company_id"], name: "index_experiences_on_company_id"
   end
 
