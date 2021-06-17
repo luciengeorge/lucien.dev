6c1a5092a8a9c9437c406b6928589194467206e8
Lucien George <luciengeorge95@gmail.com>
2021-06-17 19:28:57 +0100

    wip adding markdown

--- app/javascript/controllers/live_typing_controller.js
+++ app/javascript/controllers/live_typing_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = [ 'styleText', 'styleTag' ];
+  static targets = [ 'pre', 'style' ];
   static values = {
     openComment: Boolean
   };
@@ -13,36 +13,35 @@ export default class extends Controller {
   writeStyleChar(char) {
     // begin wrapping open comments
     let styles;
-    if (char == '/' && this.openCommentValue == false) {
+    if (char === '/' && this.openCommentValue === false) {
       this.openCommentValue = true;
-      styles = this.styleTextTarget.innerHTML + char;
-    } else if (char == '/' && this.openCommentValue == true) {
+      styles = this.preTarget.innerHTML + char;
+    } else if (char === '/' && this.openCommentValue === true) {
       this.openCommentValue = false
-      styles = this.styleTextTarget.innerHTML.replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
+      styles = this.preTarget.innerHTML.replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
     }
     // wrap style declaration
-    else if (char == ':') {
-      styles = this.styleTextTarget.innerHTML.replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
+    else if (char === ':') {
+      styles = this.preTarget.innerHTML.replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
     }
     // wrap style value
-    else if (char == ';') {
-      styles = this.styleTextTarget.innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;');
+    else if (char === ';') {
+      styles = this.preTarget.innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;');
     }
     // wrap selector
-    else if (char == '{') {
-      styles = this.styleTextTarget.innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{');
+    else if (char === '{') {
+      styles = this.preTarget.innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{');
     }
     else {
-      styles = this.styleTextTarget.innerHTML + char;
+      styles = this.preTarget.innerHTML + char;
     }
-    this.styleTextTarget.innerHTML = styles;
-    this.styleTagTarget.insertAdjacentHTML('beforeend', char);
+    this.preTarget.innerHTML = styles;
+    this.styleTarget.insertAdjacentHTML('beforeend', char);
   }
 
   writeStyles(message, index, interval) {
     if (index < message.length) {
-      const pre = this.styleTextTarget;
-      pre.scrollTop = pre.scrollHeight;
+      this.preTarget.scrollTop = this.preTarget.scrollHeight;
       this.writeStyleChar(message[index++]);
       const newInterval = this.newInterval(message, index);
       setTimeout(() => {
@@ -68,12 +67,15 @@ export default class extends Controller {
 
   styles() {
     return `/*
- * Hey! My name is Lucien George. Web Developer from Beirut, Lebanon 🇱🇧
+ * Hey! My name is Lucien George.
+ * I am a Web Developer from Beirut, Lebanon 🇱🇧
+ *
  * How are you doing today?
  *
- * Don't mind me. I'm just coding this home page at the moment.
+ * Don't mind me. I'm just coding this website at the moment.
  *
- * I'll just inject some CSS into a <style> element that I am writing right now
+ * I'll just inject some CSS into a <style> element
+ * that I am writing right now
  *
  * Let's give it a go!
  *
@@ -102,11 +104,9 @@ pre {
   max-height: 70vh;
   color: #ffffff;
   background-color: #090e15;
-  border: 1px solid #064e3b;
   padding: 32px 24px;
-  box-sizing: border-box;
   border-radius: 15px;
-  box-shadow: 0px 4px 0px 2px rgba(6, 78, 59, 0.1);
+  box-shadow: 0px 0px 10px rgba(6, 78, 59, 0.3);
 }
 
 /*
@@ -121,31 +121,48 @@ pre em:not(.comment) { font-style: normal; }
 .key           { color: #00bceb; }
 .value         { color: #ff8321; }
 
-
 /*
- * Let's build my little pen heart.
+ * Much better! Looks exactly like my text editor 👨‍💻
+ * Anw, that's not why you're here. So let's get to it.
+ * Let me tell you a little bit more about myself.
  */
 
-
-/* First, we'll move this s*** over */
-
-pre { left: 50%; }
-
 /*
- *
- * Wahoo!
- *
- * We did it!
- *
- * I mean *I* did it, but you know, whatever...
- * jake albaugh definitely did not have anything
- * to do with this.
- *
- * This pen loves CodePen!
- *
- * See you later!
- *
+ * First, we'll move this s*** over
  */
-    `;
+
+#flex {
+  transition: all 500ms;
+  width: 100%;
+  display: flex;
+  align-items: start;
+  justify-content: around;
+}
+
+pre {
+  flex: 0 1 50%;
+  margin-left: 5px;
+}
+
+#markdown {
+  transition: all 500ms;
+  flex: 0 1 50%;
+  margin-right: 50px;
+}`;
+  }
+
+  markdown() {
+    return `
+### Who am I
+I am  a lead teacher and web developer at Le Wagon London.
+I also co-founded [Impact Lebanon](https://www.impactlebanon.org), a charity aiming at helping Lebanon go through its current Financial Crisis.
+Moreover, I co-founded [krowl.io](https://www.krowl.io), a virtual workspace to help student work from home.
+
+**Some of my projects**
+- [Impact Lebanon](https://www.impactlebanon.org)
+- [Krowl](https://www.krowl.io)
+and of course,
+- [lucien-george.com](https://www.lucien-george.com)
+`;
   }
 }
