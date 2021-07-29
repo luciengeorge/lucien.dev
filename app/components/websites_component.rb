9c9836f1b690957f0516f5e07aee85b22efdb4aa
Lucien George <luciengeorge95@gmail.com>
2021-07-29 17:56:54 +0300

    fix layout

--- app/components/websites_component.rb
+++ app/components/websites_component.rb
@@ -10,7 +10,7 @@ class WebsitesComponent < MainComponent
   def klasses(index)
     case websites.size
     when 4 then 'col-span-1 row-span-1'
-    when 3 then "col-span-#{[0, 1].include?(index) ? 1 : 2} row-span-1"
+    when 3 then "col-span-1 row-span-#{[0, 2].include?(index) ? 2 : 1}"
     when 2 then 'col-span-1 row-span-2'
     when 1 then 'col-span-2 row-span-2'
     end
