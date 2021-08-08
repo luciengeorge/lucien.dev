4de7774a09a08cf22f6db878f94d4ea96e01fd36
Lucien George <luciengeorge95@gmail.com>
2021-08-08 12:20:43 +0300

    bug fix

--- app/jobs/update_repo_commits_job.rb
+++ app/jobs/update_repo_commits_job.rb
@@ -4,9 +4,8 @@ class UpdateRepoCommitsJob < ApplicationJob
   def perform(repo)
     commits = $octokit.commits(repo.gh_id)
     commits.each do |commit|
-      @commit = Commit.find_or_create_by(sha: commit.sha, url: commit.url, html_url: commit.html_url)
+      @commit = Commit.find_or_create_by(sha: commit.sha)
       @commit.update!(
-        sha: commit.sha,
         message: commit.commit&.message,
         pushed_at: commit.commit&.author&.date,
         repo: repo,
