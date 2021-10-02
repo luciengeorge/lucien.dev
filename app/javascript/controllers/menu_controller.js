aca5aaf45082f852a5ce4ba911f59f8630c5204c
Lucien George <luciengeorge95@gmail.com>
2021-10-02 10:56:34 +0100

    navbar done with new hamburger menu

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,25 +1,22 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = [ 'menu', 'face', 'cubeWrapper', 'ring', 'background', 'link' ];
-  static classes = [ 'active', 'scale' ];
+  static targets = [ 'menu', 'background', 'link', 'burger' ];
+  static classes = [ 'active', 'scale', 'open' ];
 
   toggle(event) {
     if (event.type === 'keyup' && event.key !== 'Escape') return;
 
     if (event.type === 'keyup' && event.key === 'Escape' || event.type === 'click' && event.currentTarget === this.backgroundTarget) {
       this.menuTarget.classList.remove(this.activeClass);
-      this.cubeWrapperTarget.classList.remove(this.activeClass);
-      this.ringTarget.classList.remove('active');
+      this.burgerTarget.classList.remove(this.openClass);
       setTimeout(() => {
         this.backgroundTarget.classList.add(this.scaleClass);
       }, 500);
-      this.faceTargets.forEach(target => target.classList.remove(this.activeClass));
       this.linkTargets.forEach(target => target.classList.remove(this.activeClass));
     } else {
       this.menuTarget.classList.toggle(this.activeClass);
-      this.cubeWrapperTarget.classList.toggle(this.activeClass);
-      this.ringTarget.classList.toggle(this.activeClass);
+      this.burgerTarget.classList.toggle(this.openClass);
       if (this.backgroundTarget.classList.contains(this.scaleClass)) {
         this.backgroundTarget.classList.toggle(this.scaleClass)
       } else {
@@ -27,7 +24,6 @@ export default class extends Controller {
           this.backgroundTarget.classList.toggle(this.scaleClass);
         }, 500)
       }
-      this.faceTargets.forEach(target => target.classList.toggle(this.activeClass));
       this.linkTargets.forEach(target => target.classList.toggle(this.activeClass));
     }
   }
