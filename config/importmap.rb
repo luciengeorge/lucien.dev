4aa0dc414ea6bb22d2a70e1b618694e297646577
Lucien George <luciengeorge95@gmail.com>
2022-03-06 18:13:31 +0000

    wip importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -264,3 +264,4 @@ pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.0.1/d
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "trix", to: "https://ga.jspm.io/npm:trix@2.0.0-alpha.1/dist/trix.js"
