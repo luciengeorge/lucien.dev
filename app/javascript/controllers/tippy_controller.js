4319ba15cadfe70de44b57a63f830b7dd9687381
Lucien George <luciengeorge95@gmail.com>
2022-03-06 23:13:27 +0000

    cleanup

--- app/javascript/controllers/tippy_controller.js
+++ app/javascript/controllers/tippy_controller.js
@@ -1,8 +1,5 @@
 import { Controller } from '@hotwired/stimulus';
 import tippy from 'tippy.js';
-// import 'tippy.js/dist/tippy.css';
-// import 'tippy.js/themes/light.css';
-// import 'tippy.js/animations/scale-extreme.css';
 
 export default class extends Controller {
   static values = {
