5db5eb83d517906dfc958b18ee2dd6d0a09f6a26
Lucien George <luciengeorge95@gmail.com>
2022-03-06 23:19:39 +0000

    import trix and actiontext

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails";
 import "controllers";
+import "trix";
+import "@rails/actiontext";
 import AOS from 'aos';
 import gtag from './components/analytics';
 
