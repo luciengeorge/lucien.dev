7b80ab55daae6bb7714c22bb2fa21cd3d37d8b96
Lucien George <luciengeorge95@gmail.com>
2024-03-14 11:27:57 +0000

    Fix stimulus import

--- app/javascript/controllers/alerts_controller.js
+++ app/javascript/controllers/alerts_controller.js
@@ -1,11 +1,11 @@
-import { Controller } from 'stimulus';
+import {Controller} from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = [ 'alert' ];
+  static targets = ['alert']
 
   connect() {
     setTimeout(() => {
-      this.alertTargets.forEach(alert => alert.classList.add('hidden'));
-    }, 3000);
+      this.alertTargets.forEach((alert) => alert.classList.add('hidden'))
+    }, 3000)
   }
 }
