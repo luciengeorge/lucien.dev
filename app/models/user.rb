48f025cf1b11299b357c497e67014101a163e91d
Lucien George <luciengeorge95@gmail.com>
2024-03-14 09:50:24 +0000

    Update github url

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable, :confirmable
-  GITHUB_URL = 'https://www.github.com/lucien-george'.freeze
+  GITHUB_URL = 'https://www.github.com/luciengeorge'.freeze
   LINKEDIN_URL = 'https://www.linkedin.com/in/luciengeorge'.freeze
 end
