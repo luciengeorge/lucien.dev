b28710a4b2699b4ffb3ddb2415b7c4eed946b78b
Lucien George <luciengeorge95@gmail.com>
2024-03-14 10:54:01 +0000

    Fix js error

--- app/javascript/packs/components/live_typing.js
+++ app/javascript/packs/components/live_typing.js
@@ -1,71 +1,74 @@
 const writeChar = (char, target, styleTarget = null) => {
-  let text;
+  let text
   if (styleTarget) {
-    let openCommentValue = styleTarget.dataset.openComment === 'true';
+    let openCommentValue = styleTarget.dataset.openComment === 'true'
     if (char === '/' && openCommentValue === false) {
-      styleTarget.setAttribute('data-open-comment', true);
-      text = target.innerHTML + char;
+      styleTarget.setAttribute('data-open-comment', true)
+      text = target.innerHTML + char
     } else if (char === '/' && openCommentValue === true) {
-      styleTarget.setAttribute('data-open-comment', false);
-      text = target.innerHTML.replace(/(\/[^\/]*\*)$/, '<em class="comment">$1/</em>');
-    }
-    else if (char === ':') {
-      text = target.innerHTML.replace(/([a-zA-Z- ^\n]*)$/, '<em class="key">$1</em>:');
-    }
-    else if (char === ';') {
-      text = target.innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;');
-    }
-    else if (char === '{') {
-      text = target.innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{');
+      styleTarget.setAttribute('data-open-comment', false)
+      text = target.innerHTML.replace(
+        /(\/[^\/]*\*)$/,
+        '<em class="comment">$1/</em>',
+      )
+    } else if (char === ':') {
+      text = target.innerHTML.replace(
+        /([a-zA-Z- ^\n]*)$/,
+        '<em class="key">$1</em>:',
+      )
+    } else if (char === ';') {
+      text = target.innerHTML.replace(/([^:]*)$/, '<em class="value">$1</em>;')
+    } else if (char === '{') {
+      text = target.innerHTML.replace(/(.*)$/, '<em class="selector">$1</em>{')
     } else {
-      text = target.innerHTML + char;
+      text = target.innerHTML + char
     }
-    styleTarget.insertAdjacentHTML('beforeend', char);
+    styleTarget.insertAdjacentHTML('beforeend', char)
   } else {
-    text = target.innerHTML + char;
+    text = target.innerHTML + char
   }
-  target.innerHTML = text;
+  target.innerHTML = text
 }
 
 const time = () => {
-  if (skip()) return 0;
+  if (skip()) return 0
 
-  return 16;
+  return 16
 }
 
 const newInterval = (message, index) => {
-  if (skip()) return time();
-  const comma = /\D[\,]\s$/;
-  const endOfBlock = /[^\/]\n\n$/;
-  const endOfSentence = /[\.\?\!]\s$/;
-  const slice = message.slice(index - 2, index + 1);
-  if (comma.test(slice)) return time() * 30;
-  if (endOfBlock.test(slice)) return time() * 50;
-  if (endOfSentence.test(slice)) return time() * 70;
-  return time();
+  if (skip()) return time()
+  const comma = /\D[\,]\s$/
+  const endOfBlock = /[^\/]\n\n$/
+  const endOfSentence = /[\.\?\!]\s$/
+  const slice = message.slice(index - 2, index + 1)
+  if (comma.test(slice)) return time() * 30
+  if (endOfBlock.test(slice)) return time() * 50
+  if (endOfSentence.test(slice)) return time() * 70
+  return time()
 }
 
 const writeText = (message, target, index, styleTarget, callback) => {
-  let timeoutId;
+  let timeoutId
   if (index < message.length) {
-    target.scrollTop = target.scrollHeight;
-    writeChar(message[index++], target, styleTarget);
-    const nextInterval = newInterval(message, index);
-    if(skip()) {
+    target.scrollTop = target.scrollHeight
+    writeChar(message[index++], target, styleTarget)
+    const nextInterval = newInterval(message, index)
+    if (skip()) {
       // if (timeoutId) window.clearTimeout(timeoutId);
-      writeText(message, target, index, styleTarget, callback);
+      writeText(message, target, index, styleTarget, callback)
     } else {
       timeoutId = setTimeout(() => {
-        writeText(message, target, index, styleTarget, callback);
-      }, nextInterval);
+        writeText(message, target, index, styleTarget, callback)
+      }, nextInterval)
     }
   } else {
-    callback();
+    callback()
   }
 }
 
 const skip = () => {
-  return document.querySelector('#style-text').dataset.skip === 'true';
+  return document.querySelector('#style-text')?.dataset?.skip === 'true'
 }
 
-export { writeText, skip };
+export {writeText, skip}
