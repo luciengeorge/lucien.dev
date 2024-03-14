2f6b1982453c91e4d3eeee970ffbf0cbc42da80f
Lucien George <luciengeorge95@gmail.com>
2024-03-14 09:25:14 +0000

    Bump ruby to v3.3.0

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_08_22_144231) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
